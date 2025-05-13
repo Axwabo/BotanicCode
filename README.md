3299f63beb09d098a3f4eaa15f799c939de30bf9
Axwabo <axwabo@gmail.com>
2025-05-13 13:58:52 +0200

    init project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Botanic Code
+
+A browser game (PWA) where you have to program bots to maintain a farm.
+
+# Running Locally
+
+1. Clone the repository
+2. cd into the directory `botanic-code`
+3. Execute `pnpm run dev`
---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [ vue(), VitePWA({
+        strategies: "injectManifest",
+        srcDir: "src",
+        filename: "sw.ts",
+        registerType: "prompt",
+        injectRegister: false,
+
+        pwaAssets: {
+            disabled: false,
+            config: true
+        },
+
+        manifest: {
+            name: "Botanic Code",
+            short_name: "botanic-code",
+            description: "A game where you have to program bots that maintain a farm.",
+            theme_color: "#222222"
+        },
+
+        injectManifest: {
+            globPatterns: [ "**/*.{js,css,html,svg,png,ico}" ]
+        },
+
+        devOptions: {
+            enabled: false,
+            navigateFallback: "index.html",
+            suppressWarnings: true,
+            type: "module"
+        }
+    }) ]
+})
