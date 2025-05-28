732fdbc51cc0cb7848204955debafc940b9a910d
Axwabo <axwabo@gmail.com>
2025-05-28 11:03:46 +0200

    keep track of offset chunks

--- src/game/tick.ts
+++ src/game/tick.ts
@@ -7,29 +7,36 @@ function getChunk(game: GameState, position: WorldPosition) {
     return game.board.getChunk(Math.floor(worldToChunk(position.x)), Math.floor(worldToChunk(position.y)));
 }
 
+const chunkLoadTime = 10;
+
+const offsets = [
+    [ 1, 0 ],
+    [ -1, 0 ],
+    [ 0, 1 ],
+    [ 0, -1 ],
+    [ 1, 1 ],
+    [ -1, -1 ],
+    [ 1, -1 ],
+    [ -1, 1 ]
+];
+
 function getLoadedChunks(game: GameState, deltaSeconds: number) {
     const loadedChunks = new Set<Chunk>();
     for (const bot of game.botManager.bots.values()) {
         const currentChunk = getChunk(game, bot.position);
-        bot.chunkSeconds.set(currentChunk, 10);
-        loadedChunks.add(currentChunk);
-        loadedChunks.add(game.board.getChunk(currentChunk.x + 1, currentChunk.y));
-        loadedChunks.add(game.board.getChunk(currentChunk.x - 1, currentChunk.y));
-        loadedChunks.add(game.board.getChunk(currentChunk.x, currentChunk.y + 1));
-        loadedChunks.add(game.board.getChunk(currentChunk.x, currentChunk.y - 1));
-        loadedChunks.add(game.board.getChunk(currentChunk.x + 1, currentChunk.y + 1));
-        loadedChunks.add(game.board.getChunk(currentChunk.x - 1, currentChunk.y - 1));
-        loadedChunks.add(game.board.getChunk(currentChunk.x + 1, currentChunk.y - 1));
-        loadedChunks.add(game.board.getChunk(currentChunk.x - 1, currentChunk.y + 1));
         for (const [ chunk, seconds ] of bot.chunkSeconds) {
-            if (chunk === currentChunk)
-                continue;
             const remaining = seconds - deltaSeconds;
             if (remaining <= 0)
                 bot.chunkSeconds.delete(chunk);
             else
                 bot.chunkSeconds.set(chunk, remaining);
-
+        }
+        bot.chunkSeconds.set(currentChunk, chunkLoadTime);
+        loadedChunks.add(currentChunk);
+        for (const [ x, y ] of offsets) {
+            const chunk = game.board.getChunk(currentChunk.x + x, currentChunk.y + y);
+            bot.chunkSeconds.set(chunk, chunkLoadTime);
+            loadedChunks.add(chunk);
         }
     }
     return loadedChunks;
