fbeb231f1d165d3cf4622dbed310ccffc94d98da
Axwabo <axwabo@gmail.com>
2025-07-07 15:10:00 +0200

    read-only editor overlay

--- src/components/editor/MonacoEditor.vue
+++ src/components/editor/MonacoEditor.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref } from "vue";
+import { computed, onMounted, onUnmounted, ref } from "vue";
 import * as monaco from "monaco-editor";
 import useFileStore from "../../fileStore.ts"
 import ensureMonacoEnvironment from "../../game/editor/environment.ts";
@@ -14,13 +14,15 @@ let editor: monaco.editor.IStandaloneCodeEditor | undefined;
 
 const element = ref<HTMLDivElement>();
 
+const locked = computed(() => files.get(path) === "locked");
+
 onMounted(() => {
     ensureMonacoEnvironment();
     editor = monaco.editor.create(element.value!, {
         language: path.endsWith(".ts") ? "typescript" : "javascript",
         theme: "vs-dark",
         value: instance.text,
-        readOnly: files.get(path) === "locked",
+        readOnly: locked.value,
         automaticLayout: true
     });
     editor.onDidChangeModelContent(() => files.set(path, "modified"));
@@ -34,11 +36,23 @@ onUnmounted(() => {
 </script>
 
 <template>
-    <div id="monacoEditor" ref="element"></div>
+    <div id="monacoEditor" ref="element" :class="{ locked }"></div>
 </template>
 
 <style scoped>
 #monacoEditor {
+    position: relative;
     height: 100%;
+}
+
+#monacoEditor.locked::after {
+    content: "";
+    position: absolute;
+    pointer-events: none;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: repeating-linear-gradient(135deg, transparent 1px, rgba(100, 100, 100, 0.1) 3px, transparent 4px, transparent 15px);
 }
 </style>
