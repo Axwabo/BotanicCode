71e011e95ee29ae32c8eec18ba8a671674b114ee
Axwabo <axwabo@gmail.com>
2025-07-07 16:20:42 +0200

    attempt editor libraries again

--- src/game/editor/environment.ts
+++ src/game/editor/environment.ts
@@ -2,9 +2,11 @@ import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker"
 import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker"
 import useFileStore from "../../fileStore.ts";
 import * as monaco from "monaco-editor";
+import { languages } from "monaco-editor";
 import { storeToRefs } from "pinia";
 import useSettingsStore from "../../settingsStore.ts";
 import { type Ref, watch } from "vue";
+import ModuleKind = languages.typescript.ModuleKind;
 
 const { files, get } = useFileStore();
 
@@ -18,17 +20,31 @@ export function ensureMonacoEnvironment() {
             return label === "typescript" || label === "javascript" ? new tsWorker() : new editorWorker();
         }
     };
+    monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
+        // web worker
+        target: monaco.languages.typescript.ScriptTarget.ESNext,
+        lib: [ "esnext", "webworker" ],
+        allowJs: true,
+        checkJs: true,
+        module: ModuleKind.ESNext,
+        typeRoots: [ "/bot/sdk", "/util" ]
+    });
     for (const [ path, status ] of files) {
         if (status === "locked" || status === "hidden")
             void registerLibrary(path);
     }
+    // void registerLibrary("bot/sdk/bot.js")
+    void registerLibrary("bot/sdk/requests.d.ts")
     loadSettings();
 }
 
 async function registerLibrary(path: string) {
     const response = await get(path);
-    if (response)
-        monaco.languages.typescript.javascriptDefaults.addExtraLib(response, path); // TODO: doesn't work
+    if (response) {
+        monaco.languages.typescript.javascriptDefaults.addExtraLib(response, "ts:" + path); // TODO: doesn't work
+        monaco.editor.createModel(response, "typescript", monaco.Uri.parse("ts:" + path));
+        debugger
+    }
 }
 
 function loadSettings() {
