d1bc380ea9bc33bbeefb3356b4860e0e2af4be32
Axwabo <axwabo@gmail.com>
2025-07-08 09:53:37 +0200

    auto-open example

--- src/components/tutorial/Example.vue
+++ src/components/tutorial/Example.vue
@@ -3,18 +3,21 @@ import { ref } from "vue";
 import useFileStore from "../../fileStore.ts";
 import { useTutorialStore } from "../../tutorialStore.ts";
 
-const { save } = useFileStore();
+const { save, navigate } = useFileStore();
 
 const { skip } = useTutorialStore();
 
 const loading = ref(false);
 
+const file = "/bot/main.js";
+
 async function importExample() {
     loading.value = true;
     const response = await fetch(import.meta.env.BASE_URL + "example.js");
     const text = await response.text();
-    await save("/bot/main.js", text);
+    await save(file, text);
     skip();
+    navigate(file);
 }
 
 </script>
